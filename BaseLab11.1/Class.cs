58f484b1a92f55f177e8c42f021820504fc7c138
Dmitr <Dmitr@DESKTOP-H2P4N61>
2022-03-13 21:30:27 +0200

    Добавьте файлы проекта.

---
+++ BaseLab11.1/Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLab11._2
+{
+    public class Class
+    {
+        public int First { get; set; } = 0;
+        public int Second { get; set; } = 0;
+        public int Third { get; set; } = 0;
+
+        public double Result()
+        {
+            return First * Second * Third * 1 / 2;
+        }
+
+        public string Print()
+        {
+            return $"Объем призмы : {Result()}";
+        }
+    }
+}
